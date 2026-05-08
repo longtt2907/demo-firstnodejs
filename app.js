73dbabc904fd95e80d688df35f9ce65b022f88e2
Long Long <longtt2907@gmail.com>
2026-05-08 16:49:43 +0700

    Hello word with JS

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("Hello world");
-console.log("Long Long");
+const express = require("express");
+const app = express();
+const PORT = 8080;
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+app.get("/hoidanit", (req, res) => {
+    res.send("Hello Eric!")
+})
+app.listen(PORT, () => {
+    console.log(`My app is running on port ${PORT}`)
+})
