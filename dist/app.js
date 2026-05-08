6e97bce35a9a5f718c7250e69b9338c96241f3bf
Long Long <longtt2907@gmail.com>
2026-05-08 17:04:11 +0700

    helloWorld TS

---
+++ dist/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.get("/hoidanit", (req, res) => {
+    res.send("Hello Eric!");
+});
+app.listen(PORT, () => {
+    console.log(`My app is running on port ${PORT}`);
+});
+//# sourceMappingURL=app.js.map
