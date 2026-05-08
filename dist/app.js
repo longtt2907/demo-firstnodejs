eac5bf2172640654be5fcf18a97c2c6f96b36f99
Long Long <longtt2907@gmail.com>
2026-05-08 17:19:47 +0700

    src folder

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,13 @@
-const express = require("express");
-const app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const PORT = 8080;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World Updated!");
 });
 app.get("/hoidanit", (req, res) => {
     res.send("Hello Eric!");
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 8080;
 app.get("/", (req, res) => {
-    res.send("Hello World!")
+    res.send("Hello World Updated!")
 })
 app.get("/hoidanit", (req, res) => {
     res.send("Hello Eric!")
